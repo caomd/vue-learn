0a64ed15769a299073e6cf66af8a3fe5eb6b1132
caomengdie <2676171990@qq.com>
2020-10-28 00:29:23 +0800

    day1

---
+++ vue-learn/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div v-html="msg1" v-bind:id="id"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      msg1: '<span style="color:red;">Hello world</span>',
+      id:'txt'
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
