39b4f03eb031c60fc48803476654fe7c3632e765
caomengdie <2676171990@qq.com>
2020-10-28 18:48:56 +0800

    vue event

--- vue-learn/src/components/HelloWorld.vue
+++ vue-learn/src/components/HelloWorld.vue
@@ -2,6 +2,21 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div v-html="msg1" v-bind:id="id"></div>
+    <div>{{ number + 1 }}</div>
+    <div v-if="seen">你看见我</div>
+    <div v-n="6"></div>
+    <div :class="[a1, a2]">我借你{{ money }},你还我{{ a }},我还剩下{{ b }}</div>
+    <div :class="{ 'test-1': a3 }">style object</div>
+    <div :class="obj">style obj</div>
+    <ul>
+      <li v-for="item in list" :key="item">{{ item }}</li>
+    </ul>
+    <ul>
+      <template v-for="item in list">
+        <li :key="item + 1">hello</li>
+        <li :key="item">{{ item }}</li>
+      </template>
+    </ul>
   </div>
 </template>
 
@@ -14,8 +29,24 @@ export default {
   data() {
     return {
       msg1: '<span style="color:red;">Hello world</span>',
-      id:'txt'
+      id: "txt",
+      number: 1,
+      seen: true,
+      money: 1000,
+      a: 10,
+      a1: "test-1",
+      a2: "test-2",
+      a3: false,
+      obj: {
+        "text-1": true,
+      },
+      list: ["a", "b", "c", "d"],
     };
+  },
+  computed: {
+    b: function () {
+      return this.money - this.a;
+    },
   },
 };
 </script>
---
+++ vue-learn/src/components/events.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="msg('sdd', $event)">test</button>
+    <div class="parent" @click="parent">
+        <p>parent</p>
+        <p>parent</p>
+        <p>parent</p>
+        <div class="children" @click.stop="child">
+            child
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ev",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    msg: function (s, b) {
+      alert(s);
+      console.log(b);
+    },
+    parent: function(){
+        window.console.log('parent')
+    },
+    child: function(){
+        window.console.log('child')
+
+    }
+  },
+};
+</script>
+<style scoped>
+</style>
