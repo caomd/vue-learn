49bf211ae0742ddd6c0a68b7dcdeee5b19a4d60c
caomengdie <2676171990@qq.com>
2020-10-29 16:14:30 +0800

    slot

---
+++ vue-learn/src/components/com.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>child components {{ name }} age{{age}}
+  <slot name="b"></slot>
+
+  <button @click="$emit('patch',45)">发送到父组件</button>
+  <slot name="a"></slot>
+  </div>
+</template>
+<script>
+export default {
+  props:['age',],  
+  data: function () {
+    return {
+      name: "com",
+    };
+  },
+};
+</script>
