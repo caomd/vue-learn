16904f7b45d58c6129cbe4dd62c37404eb648eab
caomengdie <2676171990@qq.com>
2020-10-30 15:52:49 +0800

    router

--- vue-learn/src/App.vue
+++ vue-learn/src/App.vue
@@ -2,16 +2,48 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg='<span style="color:#fff">Hello world</span>' />
+    <ev />
+    <com :age="age" @patch="handleChild">
+      <h1 slot="a">我要加东西32</h1>
+      <h1 slot="b">我在上边</h1>
+    </com>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import "./components/n";
+import ev from "./components/events.vue";
+import com from "./components/com.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    ev,
+    com,
+  },
+  data() {
+    return {
+      age: 10,
+    };
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  created() {
+    // 组件创建完后获取数据，
+    // 此时 data 已经被 observed 了
+    console.log("created");
+  },
+  methods: {
+    handleChild: function (d) {
+      this.age++;
+      console.log("data from child", d);
+    },
   },
 };
 </script>
---
+++ vue-learn/src/pages/b.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="page">page b</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
